f8d1183007eb7dfad337ef2cc174293c54009860
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-02-18 11:56:38 +0200

    step 4

---
+++ basic02/step3.php
@@ -0,0 +1,9 @@
+<?php
+$var1 = 32;
+$var2 = 45;
+echo " var1 = $var1, var2 = $var2\n";
+$swap = $var1;
+$var1 = $var2;
+$var2 = $swap;
+echo " var1 = $var1, var2 = $var2\n";
+?>
---
+++ basic02/step4.php
@@ -0,0 +1,22 @@
+<?php
+function isArmstrong($num) {
+  $sum = 0;
+  $num_len = strlen($num);
+  for ($i = 0; $i < $num_len; $i++) {
+    $sum += pow($num[$i], $num_len);
+  }
+  if ($sum == $num) {
+    return true;
+  } else {
+    return false;
+  }
+}
+$num1 = 153;
+$num2 = 370;
+$num3 = 371;
+$num4 = 407;
+echo "Is $num1 an Armstrong number? " . (isArmstrong($num1) ? "Yes" : "No") . "<br>";
+echo "Is $num2 an Armstrong number? " . (isArmstrong($num2) ? "Yes" : "No") . "<br>";
+echo "Is $num3 an Armstrong number? " . (isArmstrong($num3) ? "Yes" : "No") . "<br>";
+echo "Is $num4 an Armstrong number? " . (isArmstrong($num4) ? "Yes" : "No") . "<br>";
+?>
