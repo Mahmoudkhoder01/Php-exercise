1946e424a85646a657a47b4d24ccf667ec21dee2
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-02-18 09:44:56 +0200

    step 3

---
+++ basic01/step3.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Mahmoud";
+$email = "mahmoudkhodor00@gmail.com";
+echo "My name is $name and my email is $email.\n";
+$height = 70;
+$width = 180;
+$area = $height * $width;
+echo "The area is $area";
